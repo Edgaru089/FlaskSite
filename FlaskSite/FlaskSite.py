ff8a8b6baca663028fbc6a463d38aa0d976ddfb1
Edgaru089 <sjs333@outlook.com>
2018-09-27 19:50:14 +0800

    Uploaded initials

--- FlaskSite/FlaskSite.py
+++ FlaskSite/FlaskSite.py
@@ -1,1 +1,18 @@
+from flask import Flask, abort, redirect, render_template
 
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return redirect('/login')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/show_entries')
+def show_entries():
+    return render_template('show_entries.html')
+
+if __name__ == '__main__':
+    app.run(port=8080, debug=True)
